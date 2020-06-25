3f046645f54132a5f6de350eab52918c2437eed6
daniyar <daniyarsansyzbayev@protonmail.com>
2020-06-25 23:30:19 +0600

    HW One - Scala

--- src/main/scala/one/lab/tasks/week/one/Interfaces.scala
+++ src/main/scala/one/lab/tasks/week/one/Interfaces.scala
@@ -9,7 +9,7 @@ package one.lab.tasks.week.one
   *    so that when I try to play Xbox with PS game disk, it will print me that disk format is invalid.
   *    But when I supply appropriate disk it will print s"playing ${disk.game()}"
   */
-object Interfaces {
+object Interfaces extends App{
 
   trait GameDisk {
     val consoleType: String
@@ -20,12 +20,86 @@ object Interfaces {
     def play(disk: GameDisk): Unit
   }
 
+  // Game Console clasess
+
+  class Xbox extends Console {
+    override def play(disk: GameDisk): Unit = {
+      if(disk.consoleType.equals("Xbox")) {
+        println(s"playing ${disk.game}")
+      } else {
+        println(s"disk format is invalid")
+      }
+    }
+  }
+
+  class PlayStation extends Console {
+    override def play(disk: GameDisk): Unit = {
+      if(disk.consoleType.equals("PlayStation")) {
+        println(s"playing ${disk.game}")
+      } else {
+        println(s"disk format is invalid")
+      }
+    }
+  }
+
+  class Sega extends Console {
+    override def play(disk: GameDisk): Unit = {
+      if(disk.consoleType.equals("Sega")) {
+        println(s"playing ${disk.game}")
+      } else {
+        println(s"disk format is invalid")
+      }
+    }
+  }
+
+  // Console game disks
+
   trait XboxGameDisk extends GameDisk {
     override val consoleType: String = "Xbox"
+  }
+
+  trait PlayStationGameDisk extends GameDisk {
+    override val consoleType: String = "PlayStation"
+  }
+
+  trait SegaGameDisk extends GameDisk {
+    override val consoleType: String = "Sega"
+  }
+
+  // Console games
+
+  class GodOfWar extends PlayStationGameDisk {
+    override val game: String = "God of War game"
+  }
+
+  class DaysGone extends PlayStationGameDisk {
+    override val game: String = "Days Gone game"
   }
 
   class ForzaHorizon extends XboxGameDisk {
     override val game: String = "ForzaHorizon race game"
   }
+
+  class GearsOfWar extends XboxGameDisk {
+    override val game: String = "Gears of War third-person shooter game"
+  }
+
+  class Tetris extends SegaGameDisk {
+    override val game: String = "Tetris falling blocks game"
+  }
+
+  //Testing xbox games
+  new Xbox().play(new ForzaHorizon())
+  new Xbox().play(new GodOfWar())
+
+  //Testing PlayStation games
+  new PlayStation().play(new GodOfWar())
+  new PlayStation().play(new GearsOfWar())
+
+  //Testing Sega games
+  new Sega().play(new Tetris())
+  new Sega().play(new DaysGone())
+
+
 
 }
--- src/main/scala/one/lab/tasks/week/one/Recursion.scala
+++ src/main/scala/one/lab/tasks/week/one/Recursion.scala
@@ -1,11 +1,37 @@
 package one.lab.tasks.week.one
 
-object Recursion {
-  def printNTimes(n: Int, value: String): Unit = ???
+object Recursion extends App{
+  def printNTimes(n: Int, value: String): Unit = {
+    if(n==0) return //Not sure if it is ok to use it
+    print(s"$value ")
+    printNTimes(n-1, value)
+  }
+  //simple solution without recurison ==>
+  // def printNTime(n: Int, value: String): Unit = println(s"$value\n" * n)
 
-  def gcd(a: Long, b: Long): Long = ???
+  printNTimes(4, "test")
 
-  def nthFibonacciNumber(n: Int): Int = ???
+  def gcd(a: Long, b: Long): Long = {
+      if (b == 0) a
+      else gcd(b, a % b)
+  }
 
-  def tailRecursiveFibonacciNumber(n: Int): Int = ???
+  def nthFibonacciNumber(n: Int): Int = {
+    if (n <= 0) {
+      0
+    } else if (n <= 1) {
+      1
+    } else {
+      nthFibonacciNumber(n-1) + nthFibonacciNumber(n-2)
+    }
+  }
+
+  def tailRecursiveFibonacciNumber(n: Int): Int = {
+    @scala.annotation.tailrec
+    def tailFib(n: Int, prev: Int, cur: Int): Int = {
+      if (n<=0) cur
+      else tailFib(n-1, prev = prev + cur, cur = prev)
+    }
+    tailFib(n, 1, 0)
+  }
 }

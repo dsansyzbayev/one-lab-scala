78123646ece0e8aa7145c5e12b15831faedd3100
daniyar <daniyarsansyzbayev@protonmail.com>
2020-07-03 16:07:14 +0600

    File Manager global state function changed. Code Style mistakes fixed to the best of my knowledge

--- src/main/scala/one/lab/tasks/week/two/Collections.scala
+++ src/main/scala/one/lab/tasks/week/two/Collections.scala
@@ -4,7 +4,7 @@ object Collections extends App{
   // getLast(List(1 ,2, 3, 4)) -> 4
   // getLast(List())           -> java.util.NoSuchElementException
   def getLast[A](list: List[A]): A = list match {
-    case Nil => throw new NoSuchElementException()
+    case Nil       => throw new NoSuchElementException()
     case x :: tail => if (tail.isEmpty) x else getLast(tail)
   }
 
@@ -14,7 +14,7 @@ object Collections extends App{
   // getLastOption(List(1 ,2, 3, 4)) -> Some(4)
   // getLastOption(List())           -> None
   def getLastOption[A](list: List[A]): Option[A] = list match {
-    case Nil => None
+    case Nil       => None
     case x :: tail => if (tail.isEmpty) Some(x) else getLastOption(tail)
   }
 
@@ -25,11 +25,11 @@ object Collections extends App{
   // getPreLast(List(1))          -> java.util.NoSuchElementException
   // getPreLast(List())           -> java.util.NoSuchElementException
   def getPreLast[A](list: List[A]): A = list match {
-    case Nil => throw new NoSuchElementException()
+    case Nil       => throw new NoSuchElementException()
     case x :: tail => if (tail.length == 1) x else getPreLast(tail)
   }
 
-//  println(getPreLast(List(1,2,3,4)))
+//  println(getPreLast(List(1, 2, 3, 4)))
 //  println(getPreLast(List(1)))
 //  println(getPreLast(List()))
 
@@ -37,11 +37,11 @@ object Collections extends App{
   // getPreLastOption(List(1))          -> None
   // getPreLastOption(List())           -> None
   def getPreLastOption[A](list: List[A]): Option[A] = list match {
-    case Nil => None
+    case Nil       => None
     case x :: tail => if (tail.length == 1) Some(x) else getPreLastOption(tail)
   }
 
-//  println(getPreLastOption(List(1,2,3,4)))
+//  println(getPreLastOption(List(1, 2, 3, 4)))
 //  println(getPreLastOption(List(1)))
 //  println(getPreLastOption(List()))
 
@@ -50,21 +50,21 @@ object Collections extends App{
   def getNthElement[A](n: Int, list: List[Int]): Int = {
 
     def nthElem(list: List[Int], acc: (Int, Int)): Int = list match {
-      case Nil => throw new NoSuchElementException
+      case Nil       => throw new NoSuchElementException
       case x :: tail => if (acc._1 == acc._2) x else nthElem(tail, (acc._1 + 1, acc._2))
     }
     nthElem(list, (0,n))
 
   }
 
-  println(getNthElement(3, List(1 ,2, 3, 4)))
+  println(getNthElement(3, List(1, 2, 3, 4)))
 
   // getNthElementOption(3, List(1 ,2, 3, 4)) -> Some(3)
   // getNthElementOption(3, List(1))          -> None
   def getNthElementOption[A](n: Int, list: List[A]): Option[A] = {
     def nthElem(list: List[A], acc: (Int,Int)): Option[A] = list match {
-      case Nil => None
-      case x :: tail => if(acc._1 == acc._2) Some(x) else nthElem(tail, (acc._1 +1, acc._2))
+      case Nil       => None
+      case x :: tail => if (acc._1 == acc._2) Some(x) else nthElem(tail, (acc._1 +1, acc._2))
     }
     nthElem(list, (0,n))
   }
@@ -75,8 +75,8 @@ object Collections extends App{
   // getLength(List())      -> 0
   def getLength[A](list: List[A]): Int = {
     def length(list: List[A], n: Int): Int = list match {
-      case Nil => 0
-      case x :: tail => if(tail.isEmpty) n else length(tail, n+1)
+      case Nil       => 0
+      case x :: tail => if (tail.isEmpty) n else length(tail, n+1)
     }
     length(list, 1)
   }
@@ -86,8 +86,8 @@ object Collections extends App{
   // getReversedList(List(1,2,3)) -> List(3,2,1)
   def getReversedList[A](list: List[A]): List[A] = {
     def reverse(list: List[A], rev: List[A]): List[A] = list match {
-      case Nil => throw new NoSuchElementException()
-      case x :: tail => if(tail.isEmpty) x +: rev else reverse(tail, x +: rev)
+      case Nil       => throw new NoSuchElementException()
+      case x :: tail => if (tail.isEmpty) x +: rev else reverse(tail, x +: rev)
     }
     reverse(list, List())
   }
@@ -97,12 +97,12 @@ object Collections extends App{
   // duplicateEveryElement(List(1,2,3)) -> List(1,1,2,2,3,3)
   def duplicateEveryElement[A](list: List[A]): List[A] = {
     def duplicate(list: List[A], dup: List[A]): List[A] = list match {
-      case Nil => throw new NoSuchElementException()
-      case x :: tail => if(tail.isEmpty) dup ::: times(x) else duplicate(tail, dup ::: times(x) )
+      case Nil       => List.empty[A]
+      case x :: tail => if (tail.isEmpty) dup ::: times(x) else duplicate(tail, dup ::: times(x) )
     }
     def times(n: A): List[A] =  n :: n :: Nil//List.fill(2)(n)
     duplicate(list, List())
   }
 
-  println(duplicateEveryElement(List(1,2,3)))
+  println(duplicateEveryElement(List(1, 2, 3)))
 }
--- src/main/scala/one/lab/tasks/week/two/FileManager.scala
+++ src/main/scala/one/lab/tasks/week/two/FileManager.scala
@@ -27,27 +27,23 @@ object FileManager extends App {
     def isSubstitutive: Boolean = false
   }
 
-  case class PrintErrorCommand(error: String) extends Command
-  case class ListDirectoryCommand()           extends Command
-  case class ListFilesCommand()               extends Command
-  case class ListAllContentCommand()          extends Command
-  case class CurrentDirectoryCommand()        extends Command
-
-  case class OutputFileCommand(val fileName: String)             extends Command
-  case class DeleteFileCommand(val fileName: String)             extends Command
-  case class CreateNewFileCommand(val fileName: String)          extends Command
-  case class CreateNewDirectoryCommand(val directoryName: String)extends Command
-  case class ChangeDirectoryCommand(val destination: String)     extends Command {
+  case class PrintErrorCommand(error: String)                 extends Command
+  case class ListDirectoryCommand()                           extends Command
+  case class ListFilesCommand()                               extends Command
+  case class ListAllContentCommand()                          extends Command
+  case class CurrentDirectoryCommand()                        extends Command
+  case class OutputFileCommand(fileName: String)              extends Command
+  case class DeleteFileCommand(fileName: String)              extends Command
+  case class CreateNewFileCommand(fileName: String)           extends Command
+  case class CreateNewDirectoryCommand(directoryName: String) extends Command
+  case class ChangeDirectoryCommand(destination: String)      extends Command {
     override val isSubstitutive: Boolean = true
   }
 
-  case class Path(var path: String)
-
-  val currPath = Path("/home")
-
   case class ChangePathError(error: String)
+  case class FileDoesNotExistError(error: String)
 
-  def getFiles(path: String): List[String] = {
+  def getFiles(path: String): List[String] =
     Files
       .list(Paths.get(path))
       .iterator()
@@ -56,112 +52,108 @@ object FileManager extends App {
       .map(path => path.toFile.getName)
       .map(name => s"$path/$name")
       .toList
-  }
 
-  def getDirectories(path: String): List[String] = {
+  def getDirectories(path: String): List[String] =
     Files
       .list(Paths.get(path))
       .iterator()
       .asScala
       .filter(path => path.toFile.isDirectory)
-      .map(path => path.toFile.getName())
+      .map(path => path.toFile.getName)
       .map(name => s"$path/$name")
       .toList
-  }
 
-  def getAllContent(path: String): List[String] = {
+  def getAllContent(path: String): List[String] =
     Files
       .list(Paths.get(path))
       .iterator()
       .asScala
-      .map(path => path.toFile.getName())
+      .map(path => path.toFile.getName)
       .map(name => s"$path/$name")
       .toList
-  }
 
   def changePath(current: String, path: String): Either[ChangePathError, String] = {
-    if (path.equals("..")) {
-      val currSplit = current.split("/")
-      val newPath = current.substring(0, (current.length() - currSplit(currSplit.length - 1).length - 1))
-      currPath.path = newPath
-      Right(s"$newPath")
+    var cur = s"$current/$path"
+    if(path.equals("..")) {
+      cur = current.split("/").init.mkString("/")
     }
-    else {
-      val dirs = getDirectories(current)
-      if (dirs.contains(s"$current/$path")) {
-        currPath.path = s"$current/$path"
-        Right(s"$current/$path")
-      }
-      else {
-        Left(new ChangePathError(s"cd: no such file or directory: $path"))
-      }
+
+    Files.isDirectory(Paths.get(cur)) match {
+      case true  => Right(cur)
+      case false => Left(ChangePathError(s"cd: no such file or directory: $path"))
     }
   }
 
-  def getCurrentDirectory(): String = {
-    currPath.path
-  }
-
-  def createANewFile(fileName: String): String = {
-    val filePath = currPath.path
+  def createANewFile(filePath: String, fileName: String): String = {
     val path = Paths.get(s"$filePath/$fileName")
     Files.createFile(path)
     s"$fileName was created"
   }
 
-  def deleteFile(fileName: String): String = {
-    val filePath = currPath.path
-    val path = Paths.get(s"$filePath/$fileName")
-    println(path)
-    if(Files.deleteIfExists(path)) s"$fileName was deleted" else "file does not exist"
+  def deleteFile(filePath: String, fileName: String): Either[FileDoesNotExistError, String] = {
+    Files.deleteIfExists(Paths.get(s"$filePath/$fileName")) match {
+      case true  => Right(s"$fileName was deleted")
+      case false => Left(FileDoesNotExistError("file or directory does not exist"))
+    }
   }
 
-  def createDirectory(directoryName: String): String = {
-    val filePath = currPath.path
+  def createDirectory(filePath: String, directoryName: String): String = {
     val path = Paths.get(s"$filePath/$directoryName")
     Files.createDirectory(path)
     s"$directoryName was created"
   }
 
-  def outputFile(fileName: String): String = {
-    val filePath = currPath.path
-    val source =Source.fromFile(s"$filePath/$fileName")
-    val lines = try source.mkString finally source.close()
-    lines
+  def outputFile(filePath: String, fileName: String): String = {
+    val source = Source.fromFile(s"$filePath/$fileName")
+    try source.mkString finally source.close()
   }
 
   def parseCommand(input: String): Command = input match {
-    case str if str.startsWith("dir") => ListDirectoryCommand()
-    case str if str.startsWith("cd") => ChangeDirectoryCommand(str.substring(str.lastIndexOf(" ")+1))
-    case str if str.startsWith("ll") => ListAllContentCommand()
-    case str if str.startsWith("ls") => ListFilesCommand()
-    case str if str.startsWith("pwd") =>CurrentDirectoryCommand()
+    case str if str.startsWith("dir")   => ListDirectoryCommand()
+    case str if str.startsWith("cd")    => ChangeDirectoryCommand(str.substring(str.lastIndexOf(" ")+1))
+    case str if str.startsWith("ll")    => ListAllContentCommand()
+    case str if str.startsWith("ls")    => ListFilesCommand()
+    case str if str.startsWith("pwd")   => CurrentDirectoryCommand()
     case str if str.startsWith("touch") => CreateNewFileCommand(str.substring(str.lastIndexOf(" ")+1))
-    case str if str.startsWith("rm") => DeleteFileCommand(str.substring(str.lastIndexOf(" ") + 1))
+    case str if str.startsWith("rm")    => DeleteFileCommand(str.substring(str.lastIndexOf(" ") + 1))
     case str if str.startsWith("mkdir") => CreateNewDirectoryCommand(str.substring(str.lastIndexOf(" ") + 1))
-    case str if str.startsWith("cat") => OutputFileCommand(str.substring(str.lastIndexOf(" ")+1))
+    case str if str.startsWith("cat")   => OutputFileCommand(str.substring(str.lastIndexOf(" ")+1))
   }
 
   def handleCommand(command: Command, currentPath: String): String = command match {
-    case ListDirectoryCommand() => getDirectories(currentPath).mkString("\n")
-    case ListFilesCommand() => getFiles(currentPath).mkString("\n")
-    case ListAllContentCommand() => getAllContent(currentPath).mkString("\n")
-    case ChangeDirectoryCommand(destination) => changePath(currentPath, destination).toString
-    case CurrentDirectoryCommand() => getCurrentDirectory()
-    case CreateNewFileCommand(fileName) => createANewFile(fileName)
-    case DeleteFileCommand(fileName) => deleteFile(fileName)
-    case CreateNewDirectoryCommand(directoryName) => createDirectory(directoryName)
-    case OutputFileCommand(fileName) => outputFile(fileName)
+    case ListDirectoryCommand()                   => getDirectories(currentPath).mkString("\n")
+    case ListFilesCommand()                       => getFiles(currentPath).mkString("\n")
+    case ListAllContentCommand()                  => getAllContent(currentPath).mkString("\n")
+    case CurrentDirectoryCommand()                => currentPath
+    case CreateNewFileCommand(fileName)           => createANewFile(currentPath, fileName)
+    case CreateNewDirectoryCommand(directoryName) => createDirectory(currentPath, directoryName)
+    case OutputFileCommand(fileName)              => outputFile(currentPath, fileName)
+    case DeleteFileCommand(fileName)              => deleteFile(currentPath, fileName) match {
+      case Left(value)  => value.error
+      case Right(value) => value
+    }
+    case ChangeDirectoryCommand(destination)      => changePath(currentPath, destination) match {
+      case Left(value)  => value.error
+      case Right(value) => value
+    }
   }
 
   def main(basePath: String): Unit = {
-    println(s"You are in directory -> $basePath")
-    while(true) {
-      val curr = currPath.path
-      val comm = readLine()
-      println(handleCommand(parseCommand(comm), curr))
-    }
+    def innerLoop(currentPath: String): Unit = {
+      val in      = readLine()
+      val command = parseCommand(in)
+      val result  = handleCommand(command, currentPath)
 
+      println(result)
+
+      if (command.isSubstitutive) innerLoop(result)
+      else innerLoop(currentPath)
+
+      println("You are in directory ->")
+      println(s"$basePath")
+      innerLoop(basePath)
+    }
+    innerLoop(basePath)
   }
-  main(currPath.path)
+  main("/home")
 }

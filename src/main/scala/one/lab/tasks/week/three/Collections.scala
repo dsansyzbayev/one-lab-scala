73efa19d0feb156a390097dadbe6e0bc815ed596
daniyar <daniyarsansyzbayev@protonmail.com>
2020-07-11 20:27:02 +0600

    HW 3 finished - Collections and Githuber assignments finished. Scalafmt code reformater was tested

--- src/main/scala/one/lab/tasks/week/three/Collections.scala
+++ src/main/scala/one/lab/tasks/week/three/Collections.scala
@@ -1,18 +1,51 @@
 package one.lab.tasks.week.three
 
-object Collections {
+object Collections extends App{
 
   // duplicateNTimes(3, List(1,2,3)) == List(1,1,1,2,2,2,3,3,3)
   // duplicateNTimes(3, List()) == List()
-  def duplicateNTimes[A](n: Int, list: List[A]): List[A] = ???
+  def duplicateNTimes[A](n: Int, list: List[A]): List[A] = {
+    def duplicate(list: List[A], dup:List[A]): List[A] = list match {
+      case Nil       => List.empty[A]
+      case x :: tail => if (tail.isEmpty) dup ::: nTimes(n, x) else duplicate(tail, dup ::: nTimes(n, x))
+    }
+    def nTimes(n: Int, x: A): List[A] = List.fill(n)(x)
+    duplicate(list, List())
+  }
+//  println(duplicateNTimes(3, List(1,2,3)))
+//  println(duplicateNTimes(3, List()))
+
 
   // splitAtK(4, List(1,2,3,4,5,6,7,8,9)) == (List(1,2,3,4), List(5,6,7,8,9))
   // splitAtK(0, List(1,2,3)) == (List(), List(1,2,3))
-  def splitAtK[A](k: Int, list: List[A]): (List[A], List[A]) = ???
+  def splitAtK[A](k: Int, list: List[A]): (List[A], List[A]) = {
+    def split(n: Int, cList: List[A], pList: List[A]): (List[A], List[A]) = (n, cList) match {
+      case (_, Nil) => (pList.reverse, Nil)
+      case (0, list) => (pList.reverse, list)
+      case (k, x :: tail) => split(k-1, tail, x :: pList)
+    }
+    split(k, list, Nil)
+  }
+
+//  println(splitAtK(4,List(1,2,3,4,5,6,7,8,9)))
+//  println(splitAtK(0, List(1,2,3)))
+//  println(splitAtK(0, Nil))
 
   // removeKthElement(5, List(1,2,3,4,5,6)) == (List(1,2,3,4,5), 6)
   // removeKthElement(2, List(1,2,3,4,5,6)) == (List(1,2,4,5,6), 2)
   // removeKthElement(-3, List(1,2,3,4,5,6)) == IndexOutOfBoundException
   // removeKthElement(1000, List(1,2,3,4,5,6)) == IndexOutOfBoundException
-  def removeKthElement[A](k: Int, list: List[A]): (List[A], A) = ???
+  def removeKthElement[A](k: Int, list: List[A]): (List[A], A) = {
+    if (k < 0) throw new IndexOutOfBoundsException
+    def nthElem(list: List[A], acc: (Int,Int), kList: List[A]): (List[A], A) = list match {
+      case Nil       => throw new IndexOutOfBoundsException
+      case x :: tail => if (acc._1 == acc._2) ((kList ::: tail), x) else nthElem(tail, (acc._1 +1, acc._2), (kList :+ x))
+    }
+    nthElem(list, (0,k), List())
+  }
+
+//  println(removeKthElement(5, List(1,2,3,4,5,6)))
+//  println(removeKthElement(2, List(1,2,3,4,5,6)))
+//  println(removeKthElement(-3, List(1,2,3,4,5,6)))
+//  println(removeKthElement(1000, List(1,2,3,4,5,6)))
 }
--- src/main/scala/one/lab/tasks/week/three/Githuber.scala
+++ src/main/scala/one/lab/tasks/week/three/Githuber.scala
@@ -2,32 +2,64 @@ package one.lab.tasks.week.three
 
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
+import scala.concurrent.duration._
 
-import scala.concurrent.ExecutionContext
-import scala.concurrent.ExecutionContextExecutor
-import scala.concurrent.Future
+import scala.concurrent.{
+  Await,
+  ExecutionContext,
+  ExecutionContextExecutor,
+  Future
+}
 import org.json4s.DefaultFormats
+import RestClientImpl._
 import org.json4s.jackson.JsonMethods.parse
 
 import scala.util.Failure
 import scala.util.Success
 
 object Githuber extends App {
-  implicit val system: ActorSystem                        = ActorSystem("lalka")
-  implicit val materializer: ActorMaterializer            = ActorMaterializer.create(system)
-  implicit val executionContext: ExecutionContextExecutor = ExecutionContext.global
-  implicit val defaultFormats: DefaultFormats.type        = DefaultFormats
+  implicit val system: ActorSystem = ActorSystem("lalka")
+  implicit val materializer: ActorMaterializer =
+    ActorMaterializer.create(system)
+  implicit val executionContext: ExecutionContextExecutor =
+    ExecutionContext.global
+  implicit val defaultFormats: DefaultFormats.type = DefaultFormats
 
   // TODO: поля можете добавить какие хотите
-  case class GithubUser()
-  case class GithubRepository()
+  case class GithubUser(login: String, public_repos: String)
+  case class GithubRepository(name: String,
+                              size: Int,
+                              fork: Boolean,
+                              stargazers_count: Int)
 
   //  https://api.github.com/users/{$USER}
-  def getGithubUser(username: String): Future[GithubUser] = ???
+  def getGithubUser(username: String): Future[GithubUser] = {
+    val response = get(s"https://api.github.com/users/$username")(
+      system,
+      materializer,
+      executionContext)
+    response.flatMap { body =>
+      Future { parse(body).extract[GithubUser] }
+    }
+  }
 
-  def getUserRepositories(repoUrl: String): Future[List[GithubRepository]] = ???
+  def getUserRepositories(repoUrl: String): Future[List[GithubRepository]] = {
+    val response = get(repoUrl)(system, materializer, executionContext)
+    response.flatMap { body =>
+      Future { parse(body).extract[List[GithubRepository]] }
+    }
+  }
 
-  def getUserInfo(username: String): Unit = ???
+  def getUserInfo(username: String): Unit = {
+    val user = Await.result(getGithubUser(username), 5.seconds)
+    val userRepos = Await.result(
+      getUserRepositories(s"https://api.github.com/users/$username/repos"),
+      5.seconds)
+    println(s"${user.login} has ${user.public_repos} repositories")
+    for (repo <- userRepos)
+      println(
+        s"${repo.name}: has ${repo.stargazers_count} starts, ${repo.size} KB size, repo is ${if (repo.fork) "forked" else "not forked"}")
+  }
 
-  getUserInfo("")
+  getUserInfo("dsansyzbayev")
 }
